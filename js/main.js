143200210b1ac00654052879d9d5d39a5e05716e
rabiul-portfolio <rabiulawal534@gmail.com>
2024-11-24 10:45:11 +0600

    second commit

--- js/main.js
+++ js/main.js
@@ -6,10 +6,19 @@ if(navToggle){
     navToggle.addEventListener('click', ()=>{
         navMenu.classList.add('show-menu')
     })
-
+}
 if(navClose) {
     navClose.addEventListener('click', ()=>{
         navMenu.classList.remove('show-menu')
     })
 }
+
+// REMOVE MENU MOBILE
+
+const navLink = document.querySelectorAll('.nav_link')
+
+const LinkAction = ()=>{
+    const navMenu = document.getElementById('nav_menu')
+    navMenu.classList.remove('show-menu')
 }
+navLink.forEach(n => n.addEventListener('click', LinkAction))
